2f4bdbc432b3329438f033c2a52ae7231651ffea
Bovornpol Jiturai <74379360+thirds1000rr@users.noreply.github.com>
2024-08-28 13:43:20 +0700

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from mqtt import MainApp
 from cameraDetection import MainCameraApp
 
 if __name__ =="__main__" : 
-    camera = MainCameraApp()
-    camera.startCamera()
+    # camera = MainCameraApp()
+    # camera.startCamera()
     main = MainApp()
     main.start()
