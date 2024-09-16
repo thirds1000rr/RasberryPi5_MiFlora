dd633d3eb4b1bb4a58d20815f05aa07f6bf0082e
Bovornpol Jiturai <74379360+thirds1000rr@users.noreply.github.com>
2024-09-16 23:49:08 +0700

    update clean code

--- cleanup_gpio.py
+++ cleanup_gpio.py
@@ -1,26 +1,26 @@
-import gpiod
+# import gpiod
 
-class GPIOCleanup:
-    @staticmethod
-    def cleanup():
+# class GPIOCleanup:
+#     @staticmethod
+#     def cleanup():
        
-        gpio_pins = [16, 17, 27] 
+#         gpio_pins = [16, 17, 27] 
 
-        try:
-            chip = gpiod.Chip('gpiochip0')
-            for gpio in gpio_pins:
-                line = chip.get_line(gpio)
-                line.request(consumer="GPIOCleanup", type=gpiod.LINE_REQ_DIR_OUT)
-                line.set_value(1)  
-                line.release()
-                print(f"GPIO {gpio} cleaned up and set to HIGH.")
-        except Exception as e:
-            print(f"Error during GPIO cleanup: {e}")
-    def clean_specific ():
-        try : 
-            chip = gpiod.Chip('gpiochip0')
-        except Exception as e:
-            print(f"Err at clean up {e}")
+#         try:
+#             chip = gpiod.Chip('gpiochip0')
+#             for gpio in gpio_pins:
+#                 line = chip.get_line(gpio)
+#                 line.request(consumer="GPIOCleanup", type=gpiod.LINE_REQ_DIR_OUT)
+#                 line.set_value(1)  
+#                 line.release()
+#                 print(f"GPIO {gpio} cleaned up and set to HIGH.")
+#         except Exception as e:
+#             print(f"Error during GPIO cleanup: {e}")
+#     def clean_specific ():
+#         try : 
+#             chip = gpiod.Chip('gpiochip0')
+#         except Exception as e:
+#             print(f"Err at clean up {e}")
 
-if __name__ == "__main__":
-    GPIOCleanup.cleanup()
+# if __name__ == "__main__":
+#     GPIOCleanup.cleanup()
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-from gpiozero import OutputDevice
-from time import sleep
+# from gpiozero import OutputDevice
+# from time import sleep
 
-led_pin = 17
-led = OutputDevice(led_pin, active_high=False, initial_value=False)
+# led_pin = 17
+# led = OutputDevice(led_pin, active_high=False, initial_value=False)
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+# while True:
+#     led.on()
+#     sleep(1)
+#     led.off()
+#     sleep(1)
